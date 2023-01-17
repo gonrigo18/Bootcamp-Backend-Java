d5e215ea62121dd9b9868621b2fd849b496a07f7
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2023-01-17 14:12:54 -0300

    Clase1 - Tipos de variables.

---
+++ Clase1.java
@@ -0,0 +1,26 @@
+public class Clase1 {
+
+    public static void main(String[] args) {
+
+        // Tipos primitivos
+
+        // Enteros sin decimales:
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        // byte < short < int < long..
+
+        // Enteros con decimales:
+        float f;
+        double d;
+
+        // Cadenas
+        char c = 'c';
+        String nombre = "Gonzalo";
+
+        // Boolean
+        boolean flag = true;
+    }
+}
