fefeaf0458d9f1737d3fd9a6baf6ae2bda45c9c2
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2023-01-18 10:31:40 -0300

    swich

---
+++ clase2/Swich.java
@@ -0,0 +1,17 @@
+package clase2;
+
+public class Swich {
+
+    /*
+    swich (key) {
+            case value:
+                break;
+            default:
+                break;
+           }
+        */
+
+    public static void main(String[] args) {
+
+    }
+}
