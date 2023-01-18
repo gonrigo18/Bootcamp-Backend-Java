8f88d410cff993e71dd3a25c079a47b64421f850
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2023-01-18 10:06:55 -0300

    Ejercicio clase 2

---
+++ clase2/Ejercicio.java
@@ -0,0 +1,30 @@
+package clase2;
+
+import java.util.Scanner;
+
+public class Ejercicio {
+
+    // Dado 3 numeros, informar el mayor
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingresa 3 numeros!");
+        int num1 = teclado.nextInt();
+        int num2 = teclado.nextInt();
+        int num3 = teclado.nextInt();
+        int mayor;
+        mayor = Mayor(num1,num2,num3);
+        System.out.println("El numero mas grande es: " + mayor);
+    }
+
+    public static int Mayor(int a, int b, int c){
+        if (a > b && a > c) {
+            return a;
+        }else if(b > a && b > c){
+            return b;
+        }else{
+            return c;
+        }
+    }
+}
