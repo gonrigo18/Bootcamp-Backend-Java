6fda77376e79c79f3b5d7953ff789c757e7c8062
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2023-01-18 14:54:40 -0300

    for

---
+++ clase2/Ciclos.java
@@ -0,0 +1,32 @@
+package clase2;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Ciclos {
+
+    // Ingresar n
+    // sumar los pares e impares n veces.
+
+    public static void main(String[] args) {
+
+        int pares=0;
+        int impares=0;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int n = teclado.nextInt();
+
+        for (int i = 0; i < n; i++){
+            boolean esPar = i % 2 == 0;
+            if (esPar){
+                pares += i;
+            }else {
+                impares += i;
+            }
+        }
+        System.out.println(pares);
+        System.out.println(impares);
+        teclado.close();
+    }
+}
