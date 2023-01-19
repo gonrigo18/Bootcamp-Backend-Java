da4a21b95ad1a3804292d40e2181b449881e0b0b
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2023-01-19 09:00:56 -0300

    clase 3 ejercicio

---
+++ clase3/Clase3.java
@@ -0,0 +1,61 @@
+package clase3;
+
+public class Clase3 {
+    public static void main(String[] args) {
+        /*
+        for - cuando se la cantidad de veces a iterar
+        while - cuando no se la cantidad maxima
+        do while
+         */
+
+
+        /*
+        leer 6 numeros entre 100
+        cual es minimo
+        maximo
+        promedio
+        pocision de minimo
+        posicion de maximo
+         */
+
+        // vector de numeros
+        int [] numeros = new int [100];
+
+        for (int i = 0; i <100; i++){
+            numeros[i] = i;
+        }
+
+        int [] elegidos = new int [6];
+        int contador = 0;
+        int minimo=0;
+        int maximo=0;
+        float promedio;
+        int pMinimo=0;
+        int pMaximo=0;
+        int sumaNumeros=0;
+
+
+        while (contador < 6){
+            for(int i = 0; i < 6; i++){
+                elegidos[i] = numeros[i];
+                if ( i == 0 ) {
+                    minimo = i;
+                    pMinimo = i;
+                    maximo = i;
+                    pMaximo = i;
+                }
+                if(numeros[i] > maximo){
+                    maximo = numeros[i];
+                    pMaximo = i;
+                }
+                if (numeros[i] < minimo){
+                    minimo = numeros[i];
+                    pMinimo = i;
+                }
+            }
+            contador++;
+        }
+
+
+    }
+}
