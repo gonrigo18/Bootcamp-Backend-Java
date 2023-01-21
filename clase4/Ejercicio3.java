8db509b3e22d57f96e72de74ba806d77979a4ae8
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2023-01-21 11:37:22 -0300

    clase 4

--- clase4/Ejercicio3.java
+++ clase4/Ejercicio3.java
@@ -8,25 +8,39 @@ public class Ejercicio3 {
         System.out.println("Ingrese la cantidad de elementos: ");
         Scanner keyboard = new Scanner(System.in);
         int quantity = keyboard.nextInt();
-        int[] days = new int[quantity];
-        for (int i = 0; i < quantity; i++) {
-            System.out.println("Ingrese un elemento: " + i);
-            days[i] = keyboard.nextInt();
-        }
-        System.out.println("Carga finalizada");
-        System.out.println("Buscamos el mayor");
-        int mayor;
-        if (days.length > 0) {
+        if (quantity > 0) {
+            int[] days = new int[quantity];
+            for (int i = 0; i < quantity; i++) {
+                System.out.println("Ingrese un elemento: " + i);
+                days[i] = keyboard.nextInt();
+            }
+            System.out.println("Carga finalizada");
+            int mayor;
+            int pMayor;
+            int menor;
+            int pMenor;
             mayor = days[0];
+            pMayor=0;
+            menor = days[0];
+            pMenor=0;
             for (int i = 1; i < days.length; i++) {
                 if (days[i] > mayor) {
                     mayor = days[i];
+                    pMayor=i;
+                }
+                if (days[i] < menor) {
+                    menor = days[i];
+                    pMenor=i;
                 }
             }
-            System.out.println("El mayor es: " + mayor);
+            System.out.println("Buscamos el mayor...");
+            System.out.println("El mayor es: " + mayor + " en la posicion " + pMayor);
+            System.out.println("Buscamos el menor...");
+            System.out.println("El menor es: " + menor + " en la posicion " + pMenor);
         } else {
-            System.out.println("Vector vacio");
+            System.out.println("Cantidad invalida");
         }
         keyboard.close();
     }
 }
+
