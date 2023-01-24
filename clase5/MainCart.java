810185a1097ba9af01c4f85c18df2c446a498e8f
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2023-01-24 14:49:47 -0300

    Ejemplo carrito

--- clase5/MainCart.java
+++ clase5/MainCart.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 
 public class MainCart {
     public static void main(String[] args) {
-        int[] cart = new int [0];
+        int[] cart = new int[0];
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Ingrese el ID del articulo");
-        int articleId = keyboard.nextInt();
+        boolean continuar;
 
-        cart = Cart.add(cart, articleId);
+        do {
+            System.out.println("Ingrese el ID del articulo");
+            int articleId = keyboard.nextInt();
+            cart = Cart.add(cart, articleId);
+            Cart.showCart(cart);
 
-        Cart.showCart(cart);
-
-        System.out.println("Ingrese el ID del articulo");
-        articleId = keyboard.nextInt();
-        cart = Cart.add(cart, articleId);
-
-        Cart.showCart(cart);
-
+            continuar = continuar(keyboard);
+        }
+        while (continuar);
         keyboard.close();
 
+    }
+
+    static boolean continuar(Scanner keyboard) {
+        System.out.println("Desea continuar? S/N");
+
+        return keyboard.next().equalsIgnoreCase("S");
     }
 }
