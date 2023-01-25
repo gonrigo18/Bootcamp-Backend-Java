2d4a07dc2ad75ea7135a8510f07caaa085b3488a
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2023-01-24 21:14:51 -0300

    carrito

--- clase5/Cart.java
+++ clase5/Cart.java
@@ -2,9 +2,9 @@ package clase5;
 
 public class Cart {
 
-    public static Object[] add(Object[][] cart, int newId, String name) {
+    public static Object[][] add(Object[][] cart, int newId, String name) {
 
-        Object[][] newCart = new Object[cart.length + 1][1];
+        Object[][] newCart = new Object[cart.length + 1][2];
         for (int i = 0; i < cart.length; i++) {
             newCart[i][0] = cart[i][0];
             newCart[i][1] = cart[i][1];
@@ -16,9 +16,34 @@ public class Cart {
         return newCart;
     }
 
-    static void showCart(Object[][] cart) {
+    public static void showCart(Object[][] cart) {
+        System.out.println("Mostrando el carrito: ");
         for (Object[] article : cart) {
-            System.out.println("id: " + article);
+            System.out.println("id: " + article[0] + " | Articulo: " + article[1]);
         }
     }
+
+    public static Object[][] delete(Object[][] cart, int id) {
+        Object [][] newCart = new Object[cart.length-1][2];
+        int j = 0;
+        for ( int i = 0; i < cart.length; i++){
+            if ((Integer) cart[i][0] != id){
+                newCart[j][0] = cart [i][0];
+                newCart[j][1] = cart [i][1];
+                j++;
+            }
+        }
+        return newCart;
+    }
+
+    public static boolean exist (Object[][] cart, int id){
+        int idx=-1;
+        for (int i =0; idx==-1 && i < cart.length; i++){
+            if ((Integer) cart[i][0] == id){
+                idx = i;
+            }
+        }
+        return idx !=-1;
+    }
+
 }
--- clase5/MainCart.java
+++ clase5/MainCart.java
@@ -14,12 +14,23 @@ public class MainCart {
             System.out.println("Ingrese el ID del articulo");
             int articleId = keyboard.nextInt();
             String name = keyboard.next();
-            cart = Cart.add(cart, articleId,name);
+            cart = Cart.add(cart, articleId, name);
             Cart.showCart(cart);
 
             continuar = continuar(keyboard);
         }
         while (continuar);
+        System.out.println("Ingrese id a eliminar");
+        int id = keyboard.nextInt();
+        if (Cart.exist(cart, id)) {
+            cart = Cart.delete(cart, id);
+        }
+        else {
+            System.out.println("No existe el articulo");
+        }
+        Cart.showCart(cart);
+
+
         keyboard.close();
 
     }
