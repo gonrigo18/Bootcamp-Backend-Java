c0228c5618d0a65472eaac66c505c692d93f087f
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2023-01-26 21:51:52 -0300

    carrito

--- digitalersDB.sql
+++ digitalersDB.sql
@@ -10,7 +10,8 @@ stock int
 );
 
 # Muestra la tabla articulos
-select * from articulos;
+select * 
+from articulos;
 
 # Muestra el detalle de las celdas
 describe articulos; 
@@ -35,47 +36,80 @@ insert into articulos values (4,'iron man 4', 1000, 10);
 select concat(articuloid, ' - ', nombre) from articulos;
 
 # Alias 
-select articuloid as ID from articulos; 
-select concat(articuloid, ' - ', nombre) as descripcion from articulos;
-select concat_ws('|', nombre,precio,articuloid) as descripcion from articulos;
+select articuloid as ID 
+from articulos; 
+
+select concat(articuloid, ' - ', nombre) as descripcion 
+from articulos;
+
+select concat_ws('|', nombre,precio,articuloid) as descripcion 
+from articulos;
 
 # UPPER
-select  UPPER(nombre) from articulos;
+select  UPPER(nombre) 
+from articulos;
 
 # LOWER
-select  LOWER(nombre) from articulos;
+select  LOWER(nombre) 
+from articulos;
 
 # LEFT
-select left (nombre,1) as inicial from articulos;
+select left (nombre,1) as inicial 
+from articulos;
 
 # RIGHT
-select right (nombre,3) as final from articulos;
+select right (nombre,3) as final 
+from articulos;
 
 # JUNTAR FUNCIONES
-select concat(left(nombre,1), right(nombre,3)) as inicialyfinal from articulos;
+select concat(left(nombre,1), right(nombre,3)) as inicialyfinal 
+from articulos;
 
 # Limitar la cantidad de registros que obtengo en una consulta
-select * from articulos limit 2;
+select * 
+from articulos 
+limit 2;
 
 # OFFSET
-select * from articulos limit 2 offset 2;
+select * 
+from articulos 
+limit 2 
+offset 2;
 
 # Calculos
-select precio * 1.1 from articulos;
-select  sum(precio) total from articulos;
+select precio * 1.1 
+from articulos;
+
+select  sum(precio) total 
+from articulos;
 
 # Ordenamiento de datos por un solo campo
-select * from articulos order by articuloid desc;
-select * from articulos order by articuloid asc;
+select * 
+from articulos 
+order by articuloid desc;
 
-select * from articulos order by precio asc;
-select * from articulos order by precio desc;
+select * 
+from articulos 
+order by articuloid asc;
 
-select * from articulos order by precio asc, stock desc;
+select * 
+from articulos 
+order by precio asc;
+
+select * 
+from articulos 
+order by precio desc;
+
+select * 
+from articulos 
+order by precio asc, stock desc;
 
 # Actualizar un articulo 
-update articulos set precio = 1800 where articuloid = 1;
-select * from articulos;
+update articulos 
+set precio = 1800 
+where articuloid = 1;
+select * 
+from articulos;
 
 # Consulta sin tabla
 select 10 + 2;
@@ -109,10 +143,115 @@ select month (date('2021-01-26')) as mes;
 select day (date('2021-01-26')) as dia;
 
 # Encontrar un valor entre 2 valores
-select * from articulos;
-select * from articulos where precio between 100 and 3000;
-select * from articulos where precio >= 100 and precio <= 3000;
+select * 
+from articulos;
 
+select * 
+from articulos 
+where precio between 100 and 3000;
+
+select * 
+from articulos 
+where precio >= 100 and precio <= 3000;
+
+# Relacionales 
+select * 
+from articulos 
+where stock = 10;
+
+# Distintos
+select * 
+from articulos 
+where stock > 9;
+
+# Agregar columna
+alter table articulos add column fecha date;
+
+# Eliminar columna
+alter table articulos drop column fecha;
+
+# Quiero registros con fecha de creacion  diferente a null y precio mayor a 800
+select * 
+from articulos 
+where fecha is not null 
+and precio > 800;
+
+# Agregar datos
+insert into articulos values (5, 'raqueta', 1899, 12, '2023-01-25');
+insert into articulos values (6, 'libro', 1899, 12, curdate());
+insert into articulos values (7, upper('Spiderman'),1899, 12, curdate());
+
+# Obtener los articulos  con fecha entre  ayer y hoy, que tengan precio > 500, que tengan stock > 2, y ordenar los registros por fecha y precio
+select * 
+from articulos 
+where fecha between date('2023-01-24') and date ('2023-01-26') 
+and precio > 500 
+and stock > 2 
+order by fecha asc, precio asc;
+
+select articulo_id, stock 
+from articulos 
+where fecha between date('2023-01-24') and date ('2023-01-26') 
+and precio > 500 
+and stock > 2 
+order by fecha asc, precio asc;
+
+select articulo_id, stock, precio, round(precio * 1.1) as precio_10 
+from articulos 
+where fecha between date('2023-01-24') and date ('2023-01-26') 
+and precio > 500 
+and stock > 2 
+order by fecha asc, precio asc;
+
+# Case  (if)
+# si precio < 800 = barato, 
+# 800 < precio < 1000 = valor medio,
+# caro
+select nombre, precio,
+(case
+	when precio < 800 then 'Barato'
+	when precio between 800 and 1000 then 'Medio'
+	else 'Caro'
+end) as categoria 
+from articulos
+where precio < 1001
+and precio between 1000 and 3000
+order by precio asc;
+
+# IN
+select *
+from articulos
+where articulo_id = 2 or articulo_id = 3 or articulo_id = 4;
+
+select *
+from articulos
+where articulo_id >= 2 and articulo_id <= 4;
+
+select *,
+(case
+	when articulo_id = 1 then 'Heladera'
+    when articulo_id = 2 then 'Zapato'
+    else 'Es otra cosa'
+end) as tipo
+from articulos
+where articulo_id in (7,8,9,10)
+and fecha is not null
+order by precio asc;
+
+insert into articulos values (8, 'Patin', 1344, 2, curdate());
+
+select *
+from articulos;
+
+# Listar todos los articulos incluyendo una columna denominada precio con iva 
+select *, (precio * 1.21) as precio_iva
+from articulos;
+
+# Listar todos los articulos incluyendo una columna denominada  "cantidad de cuotas"
+# y otra "valor de cuota", la cantidad es fija  y es 3, el valor de cuota corresponde a 1/3 del monto con un 5% de interes
+
+select *, 3 as cant_cuotas, round(((1/3*precio)*1.05)) as valor_cuota, round((((1/3*precio)*1.05))*3) as total_a_pagar
+from articulos;
 
 
 
